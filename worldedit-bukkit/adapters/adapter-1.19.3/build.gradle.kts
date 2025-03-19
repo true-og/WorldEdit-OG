f63056e8164b14b857611cfbdabded9e06ecef93
NotAlexNoyle <alexnoyle@icloud.com>
2025-03-19 23:44:35 +0000

    Fix build for 1.19.4

--- worldedit-bukkit/adapters/adapter-1.19.3/build.gradle.kts
+++
@@ -1,6 +0,0 @@
-applyPaperweightAdapterConfiguration()
-
-dependencies {
-    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperDevBundle("1.19.3-R0.1-20221226.180038-55")
-}
--- worldedit-bukkit/adapters/adapter-1.19.4/build.gradle.kts
+++ worldedit-bukkit/adapters/adapter-1.19.4/build.gradle.kts
@@ -2,5 +2,5 @@ applyPaperweightAdapterConfiguration()
 
 dependencies {
     // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
-    paperDevBundle("1.19.4-R0.1-20230315.180941-2")
+    paperDevBundle("1.19.4-R0.1-SNAPSHOT")
 }
